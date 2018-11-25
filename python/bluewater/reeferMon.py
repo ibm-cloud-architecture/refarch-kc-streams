d09fe3eb6ca740982aa2174e035c7778f890a149
martin siegenthaler <martin@siegenthaler.org>
2018-11-24 22:38:58 -0800

    Move toward Conform to the PEP standard

--- python/bluewater/reeferMon.py
+++ python/bluewater/reeferMon.py
@@ -7,56 +7,50 @@ Monitor shipboard reefers (refrigerated containers) traveling over blue water.
 *****
 NOTES
 *****
-- Pushing data to redis in order that something viewport into the processing.
 
+- Pushing data to redis in order that something viewport into the processing.
+- Sample record
+   type: <class 'dict'>
+   data: {'id': 'Reefer_5', 'ts': '2018-01-01 21:44:00',
+       'oTemp': 22.1,
+       'latitude': 10.11291508, 'longitude': 83.09646606,
+       'amp': 55.730234304187434, 'tempC': 1.974183120145919}
 
 """
 
 import common
 import credential
-
-from streamsx.topology.topology import *
+import argparse
 from streamsx.topology.schema import *
-
 from streamsx.topology.topology import Topology
 from streamsx.topology.schema import CommonSchema
-from streamsx.topology.context import submit
 import streamsx.messagehub as messagehub
-
 from pathlib import Path
-
-import time
-import datetime
-import argparse
 from resourceAccess import TransmitRedis
 
-class exampleMap(object):
-    def __init__(self, valvar):
-        self.valvar = valvar
+
+class ExampleMap(object):
+    def __init__(self, val_var):
+        self.valvar = val_var
         pass
-    def __call__(self, dict):
-        print("type:", type(dict))
-        print("data:", dict)
-        return dict
 
-"""
-Sample record
-type: <class 'dict'>
-data: {'id': 'Reefer_5', 'ts': '2018-01-01 21:44:00', 'oTemp': 22.1, 'latitude': 10.11291508, 'longitude': 83.09646606, 'amp': 55.730234304187434, 'tempC': 1.974183120145919}
-"""
+    def __call__(self, dct):
+        print("type:", type(dct))
+        print("data:", dct)
+        return dct
 
 
+def monitor(job_name, name_space, mh_topic, redis_base=None):
+    topo = Topology(job_name, name_space)
 
-def monitor(jobName, nameSpace, mhTopic, redisBase=None):
-    topo = Topology(jobName, nameSpace)
+    fromMh = messagehub.subscribe(topo, schema=CommonSchema.Json, topic=mh_topic)
 
-    from_mh = messagehub.subscribe(topo, schema=CommonSchema.Json, topic=mhTopic)
-
-    examMap = from_mh.map(exampleMap(valvar=10), name="examMap")
-    filterTest = examMap.filter(lambda t: t['id'].startswith("Reefer_"), name="filterTest")
+    exampleMap = fromMh.map(ExampleMap(val_var=10), name="examMap")
+    filterTest = exampleMap.filter(lambda t: t['id'].startswith("Reefer_"), name="filterTest")
     filterLambda = filterTest.filter(lambda t: t['id'] is not None, name="anaTest")
-    mapLambda = filterLambda.map(lambda t: dict((k, t[k]) for k in ("id", "oTemp")))
-    mapLambda.sink(TransmitRedis(credentials=credential.redisCredential, destKey=redisBase + "/bluewater", chunkCount=10))
+    # mapLambda = filterLambda.map(lambda t: dict((k, t[k]) for k in ("id", "oTemp")))
+    filterLambda.sink(TransmitRedis(credentials=credential.redisCredential,
+                                     dest_key=redis_base + "/bluewater", chunk_count=1000))
     return topo
 
 
@@ -64,35 +58,35 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Reefer Monitor')
     parser.add_argument('--pwd', help="password to decrypyt credential file", default=None)
     parser.add_argument('--streamsService', help="Name of the Streams Analytics service", default="Streaming3Turbine")
-    parser.add_argument('--serviceType', help="Type of service to build: STREAMING_ANALYTICS, BUILD_ARCHIVE.", default="STREAMING_ANALYTICS")
+    parser.add_argument('--serviceType', help="Type of service to build: STREAMING_ANALYTICS, BUILD_ARCHIVE.",
+                        default="STREAMING_ANALYTICS")
     parser.add_argument('--buildType',
                         help="Either 'DISTRIBUTED' or 'BUNDLE' determines if scripts+submit or just scripts.",
                         default="DISTRIBUTED")
     defJobName = Path(parser.prog).resolve().stem
     parser.add_argument('--jobName', help="Name to assign to the job name", default=defJobName)
     parser.add_argument('--nameSpace', help="Name to assign to the namespace", default=defJobName)
-    parser.add_argument('--cancel', help="Cancel active job before submitting job, uses jobName, nameSpace", default=True)
-    ## application specfic arguments...
+    parser.add_argument('--cancel', help="Cancel active job before submitting job, uses jobName, nameSpace",
+                        default=True)
+    # application specfic arguments...
     parser.add_argument('--mhTopic', help="MessageHub topic to to send ekg events out on.", default="jsonEvents")
     parser.add_argument('--redisBase', help="Redis monitor path base path.", default="/score")
 
     args = parser.parse_args()
 
-    topo = monitor(jobName=args.jobName, nameSpace=args.nameSpace,
-                   mhTopic=args.mhTopic, redisBase=args.redisBase)
+    topology = monitor(job_name=args.jobName, name_space=args.nameSpace,
+                   mh_topic=args.mhTopic, redis_base=args.redisBase)
 
     try:
         import creds.credential as creds
     except ImportError:
-        common.decryptCredentials(zipPath="../shared/creds/", cryptFile="credential.py.zip", decryptFile='credential.py', pwd=args.pwd)
+        common.decryptCredentials(zipPath="../shared/creds/", cryptFile="credential.py.zip",
+                                  decryptFile='credential.py', pwd=args.pwd)
         import creds.credential as creds
-    submitStatus = common.submitProcess(topology=topo,
-          streamsService=args.streamsService,
+    submitStatus = common.submitProcess(topology=topology,
+                                        streamsService=args.streamsService,
           buildType=args.buildType,
           serviceType=args.serviceType,
           jobName=args.jobName,
           cancel=args.cancel, )
     print("Process status:%s" % submitStatus)
-
-
-
--- python/shared/resourceAccess.py
+++ python/shared/resourceAccess.py
@@ -3,9 +3,9 @@ import json
 import credential
 import collections
 
-class TransmitRedis(object):
 
-    def __init__(self, credentials, destKey, fieldsStr=None, chunkCount=None):
+class TransmitRedis(object):
+    def __init__(self, credentials, dest_key, fields_str=None, chunk_count=None):
         """
        Write dict to Redis based upon the destKey.
        If the objectFieldKey True, then the dest key
@@ -15,18 +15,18 @@ class TransmitRedis(object):
        FieldsStr only applies to dict anything else being generated will
        result in a runtime error.
         :param credentials: redis credentials
-        :param destKey: redis path
-        :param fieldsStr: comm separated list of fields to send if None, send them all.
-        :param chunkCount: previous - 1 tuples to send with the arrival of each new tuple,
+        :param dest_key: redis path
+        :param fields_str: comm separated list of fields to send if None, send them all.
+        :param chunk_count: previous - 1 tuples to send with the arrival of each new tuple,
         """
         self.credentials = credentials
-        self.destKey = destKey
+        self.destKey = dest_key
         self.fieldsStr = None
-        self.chunkCount = chunkCount
+        self.chunkCount = chunk_count
 
-        if fieldsStr is not None:
-            self.fieldsStr = fieldsStr.split(",")
-            print("Redis destination:", destKey, "fields specified:", self.fieldsStr )
+        if fields_str is not None:
+            self.fieldsStr = fields_str.split(",")
+            print("Redis destination:", dest_key, "fields specified:", self.fieldsStr)
         self.redisHandle = None
 
     def __enter__(self):
@@ -35,24 +35,23 @@ class TransmitRedis(object):
                                        password=self.credentials['password'])
         if self.chunkCount is not None:
             self.chunk = collections.deque(maxlen=self.chunkCount)
-        return(self)
+        return self
 
-    def __call__(self, sendDict):
-        sendData = sendDict
+    def __call__(self, send_dct):
+        send = send_dct
         if self.fieldsStr is not None:
-            sendData = {k: sendDict[k] for k in self.fieldsStr}
+            send = {k: send_dct[k] for k in self.fieldsStr}
         if self.chunkCount is not None:
-            self.chunk.append(sendData)
-            sendData = list(self.chunk)
-        self.redisHandle.set(self.destKey, json.dumps(sendData))
+            self.chunk.append(send)
+            send = list(self.chunk)
+        self.redisHandle.set(self.destKey, json.dumps(send))
 
     def __exit__(self, type, value, traceback):
         return False
 
+
 if __name__ == '__main__':
-    with TransmitRedis(credential.redisCredential, "/score/bluewater", chunkCount=10) as trans:
+    with TransmitRedis(credential.redisCredential, "/score/bluewater", chunk_count=10) as trans:
         lst = list()
         for idx in range(10):
-            trans({"idx":idx, "idxString":"idx_"+str(idx)})
-
-
+            trans({"idx": idx, "idxString": "idx_"+str(idx)})
