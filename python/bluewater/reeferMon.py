f29b980dd51df4ba857cc6d6b87141116474e177
martin siegenthaler <martin@siegenthaler.org>
2018-11-27 19:46:59 -0800

    Add unit failure processing.

--- python/bluewater/reeferMon.py
+++ python/bluewater/reeferMon.py
@@ -18,35 +18,13 @@ NOTES
 
 """
 
-import common
 import credential
-import argparse
 from streamsx.topology.schema import *
 from streamsx.topology.topology import Topology
 from streamsx.topology.schema import CommonSchema
 import streamsx.messagehub
-from pathlib import Path
 from resourceAccess import TransmitRedis
 from random import random
-
-
-
-
-def augment_weather(iDict):
-    # TODO use lat/long when
-    """
-    Put some weather data into the dictionary
-    :param iDict:
-    :return:
-    """
-    iDict['weatherC'] = (random() * 10) + 10
-    return(iDict)
-
-def format_fire(iDict):
-    iDict['severity'] = "fire"
-    iDict['issue'] = "combustion within container"
-    iDict['status'] = "active"
-    return(iDict)
 
 
 
@@ -56,95 +34,129 @@ class TagTuple(object):
 
     def __call__(self, in_dict):
         in_dict['tag'] = self.tag
-        print(in_dict)
         return in_dict
+
+def augment_weather(iDict):
+    # TODO lat/long in place
+    """
+    Put some weather data into the dictionary
+    :param iDict:
+    :return:
+    """
+    iDict['weatherC'] = (random() * 10) + 10
+    return(iDict)
+
+def format_heatwave(idict):
+    idict['severity'] = "situational"
+    idict['issue'] = "heatwave"
+    idict['status'] = "top units get hotter"
+    return idict
+
+
+def format_unitDown(idict):
+    idict['severity'] = "down"
+    idict['issue'] = "unit is no longer functioning "
+    idict['status'] = "temp exceeded and 0 amps"
+    return idict
+
+
+def format_fire(idict):
+    idict['severity'] = "fire"
+    idict['issue'] = "combustion within container"
+    idict['status'] = "active"
+    return idict
 
 
 class Consolidate(object):
     """
     Consolidate interleaved messages : 'ship', 'container'
 
-    'ship' messages data is stored in a dictionary, the data is not propated
-    'container' uses 'shipId' to extract the stored message data.
+    'ship' messages {lat,long} is stored in a dictionary, the tuple is not propagated
+    'container' uses 'shipId' to extract the stored message {lat, long}
     """
 
     def __init__(self):
         self.shipRegister = dict()
         pass
 
-    def __call__(self, indict):
-        print("consolidate", indict)
-        if indict['tag'] == "ship":
-            self.shipRegister[indict['shipId']] = {"longitude": indict["longitude"], "latitude": indict['latitude']}
+    def __call__(self, idict):
+        if idict['tag'] == "ship":
+            self.shipRegister[idict['shipId']] = {"longitude": idict["longitude"], "latitude": idict['latitude']}
             return None
         # we are processing a container
-        enrich = self.shipRegister.get(indict['shipId'],{"latitude": 0.0, "longitude": 0.0})
-        indict.update(enrich)
-        return indict
+        enrich = self.shipRegister.get(idict['shipId'], {"latitude": 0.0, "longitude": 0.0})
+        idict.update(enrich)
+        return idict
+
+
+""" Sample Recod
+{'shipId': 'medusa', 'ts': '2018-01-01 00:06:00', 'amp': 0.0, 'weatherC': 12.808168421380408, 'tempC': -3.8947368077106654, 'latitude': 10.76488498, 'tag': 'container', 'containerId': 'Reefer_2', 'longitude': 78.71498106}
+"""
+
+
+class UnitDown():
+    def __init__(self, temp_high=30, amp_low=0):
+        self.fleet = dict()
+        self.tempHigh = temp_high
+        self.tempInit = temp_high - 2
+        self.ampLow = amp_low
+        self.ampInit = amp_low + 2
+        pass
+
+    def __call__(self, idict):
+        fault = False
+        t, a = idict['tempC'], idict['amp']
+        # extract ship from fleet, extract container from ship
+        ship = self.fleet.get(idict['shipId'], dict())
+        cont = ship.get(idict['containerId'], {'tempC': self.tempInit,'amp': self.ampInit })
+
+        if t > self.tempHigh and a <= self.ampLow and cont['tempC'] >self.tempHigh and cont['amp'] <= self.ampLow :
+            fault = True
+        # update for container, update the container of the ship , update the ship of the fleet
+        cont.update({'amp':a, 'tempC':t})
+        ship.update({idict['containerId']: cont})
+        self.fleet.update({idict['shipId']: ship})
+        return fault
+
+class Heatwave():
+    def __init__(self, ):
+        pass
+
+    def __call__(self, idict):
+        #return idict
+        return False
 
 
 
 def monitor(job_name, name_space, mh_topic, redis_base=None):
     topo = Topology(job_name, name_space)
     topo.add_pip_package('streamsx.messagehub')
+    # fetch and tag tuples
     shipMh = streamsx.messagehub.subscribe(topo, schema=CommonSchema.Json, topic="bluewaterShip", name="shipMH")
     shipMh = shipMh.map(TagTuple("ship"), name="shipTag")
     containerMh = streamsx.messagehub.subscribe(topo, schema=CommonSchema.Json, topic="bluewaterContainer",
                                                 name="containerMH")
     containerMh = containerMh.map(TagTuple("container"), name="containerTag")
-
-
+    # normalize the tuples
     interLeaved = shipMh.union({containerMh})
-
     consolidated = interLeaved.map(Consolidate(), name="consolidate")
-
     complete = consolidated.map(augment_weather, name="weatherAugment")
 
-    filterFire = complete.filter(lambda t: t['tempC'] > 200.0 , name="fireTest")
-    formatFire = filterFire.map(format_fire, name="fireFormat")
+    complete.print(tag="complete")
+    # process the data
+    heatwaveFiltered = complete.filter(Heatwave(), name="heatwaveTest")
+    formatHeatwave = heatwaveFiltered.map(format_heatwave, name="heatwaveFmt")
+    unitDownFiltered = complete.filter(UnitDown(), name="downTest")
+    formatDown = unitDownFiltered.map(format_unitDown, name="downFmt")
+    fireFiltered = complete.filter(lambda t: t['tempC'] > 200.0 , name="fireTest")
+    formatFire = fireFiltered.map(format_fire, name="fireFmt")
 
-    formatFire.sink(TransmitRedis(credentials=credential.redisCredential,
-                                     dest_key=redis_base + "/bluewater/fire", chunk_count=100), name="fireRedis")
-
-    messageFire = formatFire.as_json(name="fireJson")
-    streamsx.messagehub.publish(messageFire, topic="bluewaterProblem", name="problemMH")
+    # colsolidate notification - redis + messagehub
+    formatted = formatFire.union({formatDown, formatHeatwave})
+    formatted.sink(TransmitRedis(credentials=credential.redisCredential,
+                                     dest_key=redis_base + "/bluewater/notify", chunk_count=100), name="notifyRedis")
+    messageProblem = formatted.as_json(name="castJson")
+    streamsx.messagehub.publish(messageProblem, topic="bluewaterProblem", name="problemMH")
 
     return topo
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Reefer Monitor')
-    parser.add_argument('--pwd', help="password to decrypyt credential file", default=None)
-    parser.add_argument('--streamsService', help="Name of the Streams Analytics service", default="Streaming3Turbine")
-    parser.add_argument('--serviceType', help="Type of service to build: STREAMING_ANALYTICS, BUILD_ARCHIVE.",
-                        default="STREAMING_ANALYTICS")
-    parser.add_argument('--buildType',
-                        help="Either 'DISTRIBUTED' or 'BUNDLE' determines if scripts+submit or just scripts.",
-                        default="DISTRIBUTED")
-    defJobName = Path(parser.prog).resolve().stem
-    parser.add_argument('--jobName', help="Name to assign to the job name", default=defJobName)
-    parser.add_argument('--nameSpace', help="Name to assign to the namespace", default=defJobName)
-    parser.add_argument('--cancel', help="Cancel active job before submitting job, uses jobName, nameSpace",
-                        default=True)
-    # application specfic arguments...
-    parser.add_argument('--mhTopic', help="MessageHub topic to to send ekg events out on.", default="jsonEvents")
-    parser.add_argument('--redisBase', help="Redis monitor path base path.", default="/score")
-
-    args = parser.parse_args()
-
-    topology = monitor(job_name=args.jobName, name_space=args.nameSpace,
-                   mh_topic=args.mhTopic, redis_base=args.redisBase)
-
-    try:
-        import creds.credential as creds
-    except ImportError:
-        common.decryptCredentials(zipPath="../shared/creds/", cryptFile="credential.py.zip",
-                                  decryptFile='credential.py', pwd=args.pwd)
-        import creds.credential as creds
-    submitStatus = common.submitProcess(topology=topology,
-                                        streamsService=args.streamsService,
-          buildType=args.buildType,
-          serviceType=args.serviceType,
-          jobName=args.jobName,
-          cancel=args.cancel, )
-    print("Process status:%s" % submitStatus)
