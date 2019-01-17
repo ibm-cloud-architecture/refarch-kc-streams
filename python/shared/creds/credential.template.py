4456af3225eccb27d0ccf94eea45bdf2b0e47b65
fnparr <31744492+fnparr@users.noreply.github.com>
2019-01-17 16:49:46 -0500

    credential.template.py  is better file naming

---
+++ python/shared/creds/credential.template.py
@@ -0,0 +1,12 @@
+streaming3Turbine = {
+  "apikey":                 "XXXXXXXXXX",
+  "iam_apikey_description": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "iam_apikey_name":        "XXXXXXXXXXXXXXXXXXXX",
+  "iam_role_crn":           "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "iam_serviceid_crn":      "XXXXXXXXXXXXXXXXXXXXXXXXX",
+  "v2_rest_url":            "XXXXXXXXXXXXXXXX"
+}
+
+StreamsServices = {
+    "Streaming3Turbine": streaming3Turbine
+}
